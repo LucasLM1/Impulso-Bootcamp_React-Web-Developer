f2edbeddfa144b14f378c99bcb242a501fde3f68
LucasLM1 <lucascorreia325@gmail.com>
2021-10-06 21:01:06 -0300

    atividade prática de módulos

---
+++ modulos/main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade, mostraCidade, mostraHobby } from './functions.mjs';
+
+console.log(mostraIdade('Lucas', 19));
+
+console.log(mostraCidade('Rapha', 'São Paulo'));
+
+console.log(mostraHobby('Paulo', 'Comer'));
